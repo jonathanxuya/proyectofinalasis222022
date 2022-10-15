118b4636163bdac291ea7b0ae049e5ba75dfed84
Jonathan Xuyá <jxuyax@miumg.edu.gt>
2022-10-14 23:34:55 -0600

    Subiendo manteniemento activos

--- Codigo/Modulos/Contabilidad/ModuloContabilidadd/Activos.Designer.cs
+++ Codigo/Modulos/Contabilidad/ModuloContabilidadd/Activos.Designer.cs
@@ -36,10 +36,22 @@ namespace ModuloContabilidadd
             this.iconButton25 = new FontAwesome.Sharp.IconButton();
             this.iconButton8 = new FontAwesome.Sharp.IconButton();
             this.iconButton3 = new FontAwesome.Sharp.IconButton();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.label31 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtExistencias = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPrecioPublico = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel12.SuspendLayout();
             this.panel18.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel12
@@ -49,22 +61,21 @@ namespace ModuloContabilidadd
             this.panel12.Location = new System.Drawing.Point(13, 13);
             this.panel12.Margin = new System.Windows.Forms.Padding(4);
             this.panel12.Name = "panel12";
-            this.panel12.Size = new System.Drawing.Size(980, 361);
+            this.panel12.Size = new System.Drawing.Size(1143, 575);
             this.panel12.TabIndex = 2;
             // 
             // panel18
             // 
+            this.panel18.Controls.Add(this.groupBox1);
             this.panel18.Controls.Add(this.iconButton26);
             this.panel18.Controls.Add(this.iconButton12);
             this.panel18.Controls.Add(this.iconButton25);
             this.panel18.Controls.Add(this.iconButton8);
             this.panel18.Controls.Add(this.iconButton3);
-            this.panel18.Controls.Add(this.richTextBox1);
-            this.panel18.Controls.Add(this.label31);
-            this.panel18.Location = new System.Drawing.Point(24, 23);
+            this.panel18.Location = new System.Drawing.Point(26, 23);
             this.panel18.Margin = new System.Windows.Forms.Padding(4);
             this.panel18.Name = "panel18";
-            this.panel18.Size = new System.Drawing.Size(905, 276);
+            this.panel18.Size = new System.Drawing.Size(1080, 529);
             this.panel18.TabIndex = 0;
             // 
             // iconButton26
@@ -74,7 +85,7 @@ namespace ModuloContabilidadd
             this.iconButton26.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton26.IconSize = 60;
             this.iconButton26.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.iconButton26.Location = new System.Drawing.Point(760, 30);
+            this.iconButton26.Location = new System.Drawing.Point(878, 91);
             this.iconButton26.Margin = new System.Windows.Forms.Padding(4);
             this.iconButton26.Name = "iconButton26";
             this.iconButton26.Size = new System.Drawing.Size(94, 84);
@@ -82,6 +93,7 @@ namespace ModuloContabilidadd
             this.iconButton26.Text = "Update";
             this.iconButton26.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.iconButton26.UseVisualStyleBackColor = true;
+            this.iconButton26.Click += new System.EventHandler(this.iconButton26_Click);
             // 
             // iconButton12
             // 
@@ -89,7 +101,7 @@ namespace ModuloContabilidadd
             this.iconButton12.IconColor = System.Drawing.Color.DimGray;
             this.iconButton12.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton12.IconSize = 50;
-            this.iconButton12.Location = new System.Drawing.Point(650, 122);
+            this.iconButton12.Location = new System.Drawing.Point(768, 183);
             this.iconButton12.Margin = new System.Windows.Forms.Padding(4);
             this.iconButton12.Name = "iconButton12";
             this.iconButton12.Size = new System.Drawing.Size(92, 84);
@@ -97,6 +109,7 @@ namespace ModuloContabilidadd
             this.iconButton12.Text = "Elimiar";
             this.iconButton12.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.iconButton12.UseVisualStyleBackColor = true;
+            this.iconButton12.Click += new System.EventHandler(this.iconButton12_Click);
             // 
             // iconButton25
             // 
@@ -104,7 +117,7 @@ namespace ModuloContabilidadd
             this.iconButton25.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(180)))), ((int)(((byte)(227)))));
             this.iconButton25.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton25.IconSize = 60;
-            this.iconButton25.Location = new System.Drawing.Point(648, 30);
+            this.iconButton25.Location = new System.Drawing.Point(766, 91);
             this.iconButton25.Margin = new System.Windows.Forms.Padding(4);
             this.iconButton25.Name = "iconButton25";
             this.iconButton25.Size = new System.Drawing.Size(94, 84);
@@ -112,6 +125,7 @@ namespace ModuloContabilidadd
             this.iconButton25.Text = "Buscar";
             this.iconButton25.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.iconButton25.UseVisualStyleBackColor = true;
+            this.iconButton25.Click += new System.EventHandler(this.iconButton25_Click);
             // 
             // iconButton8
             // 
@@ -119,7 +133,7 @@ namespace ModuloContabilidadd
             this.iconButton8.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(50)))), ((int)(((byte)(52)))));
             this.iconButton8.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton8.IconSize = 60;
-            this.iconButton8.Location = new System.Drawing.Point(523, 122);
+            this.iconButton8.Location = new System.Drawing.Point(641, 183);
             this.iconButton8.Margin = new System.Windows.Forms.Padding(4);
             this.iconButton8.Name = "iconButton8";
             this.iconButton8.Size = new System.Drawing.Size(100, 84);
@@ -134,7 +148,7 @@ namespace ModuloContabilidadd
             this.iconButton3.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(180)))), ((int)(((byte)(227)))));
             this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton3.IconSize = 60;
-            this.iconButton3.Location = new System.Drawing.Point(523, 30);
+            this.iconButton3.Location = new System.Drawing.Point(641, 91);
             this.iconButton3.Margin = new System.Windows.Forms.Padding(4);
             this.iconButton3.Name = "iconButton3";
             this.iconButton3.Size = new System.Drawing.Size(100, 84);
@@ -142,37 +156,156 @@ namespace ModuloContabilidadd
             this.iconButton3.Text = "Agregar";
             this.iconButton3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.iconButton3.UseVisualStyleBackColor = true;
+            this.iconButton3.Click += new System.EventHandler(this.iconButton3_Click);
             // 
-            // richTextBox1
+            // groupBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(32, 49);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(473, 185);
-            this.richTextBox1.TabIndex = 30;
-            this.richTextBox1.Text = "";
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.txtExistencias);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtPrecioPublico);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(31, 39);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(602, 400);
+            this.groupBox1.TabIndex = 36;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Catalogo de cuentas";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label31
+            // txtId
             // 
-            this.label31.AutoSize = true;
-            this.label31.Location = new System.Drawing.Point(28, 18);
-            this.label31.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label31.Name = "label31";
-            this.label31.Size = new System.Drawing.Size(138, 17);
-            this.label31.TabIndex = 0;
-            this.label31.Text = "Catálogo de cuentas";
+            this.txtId.Location = new System.Drawing.Point(417, 260);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(132, 30);
+            this.txtId.TabIndex = 13;
+            this.txtId.Visible = false;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(404, 58);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 31);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtExistencias
+            // 
+            this.txtExistencias.Location = new System.Drawing.Point(184, 326);
+            this.txtExistencias.Margin = new System.Windows.Forms.Padding(4);
+            this.txtExistencias.Name = "txtExistencias";
+            this.txtExistencias.Size = new System.Drawing.Size(199, 30);
+            this.txtExistencias.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 331);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Existencias:";
+            // 
+            // txtPrecioPublico
+            // 
+            this.txtPrecioPublico.Location = new System.Drawing.Point(184, 263);
+            this.txtPrecioPublico.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrecioPublico.Name = "txtPrecioPublico";
+            this.txtPrecioPublico.Size = new System.Drawing.Size(199, 30);
+            this.txtPrecioPublico.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 268);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Precio público:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(184, 165);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(332, 73);
+            this.txtDescripcion.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Descripción:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(184, 110);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(332, 30);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(184, 55);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(199, 30);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
             // 
             // Activos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 418);
+            this.ClientSize = new System.Drawing.Size(1231, 615);
             this.Controls.Add(this.panel12);
             this.Name = "Activos";
             this.Text = "Activos";
             this.panel12.ResumeLayout(false);
             this.panel18.ResumeLayout(false);
-            this.panel18.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -186,7 +319,18 @@ namespace ModuloContabilidadd
         private FontAwesome.Sharp.IconButton iconButton25;
         private FontAwesome.Sharp.IconButton iconButton8;
         private FontAwesome.Sharp.IconButton iconButton3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Label label31;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtExistencias;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPrecioPublico;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
     }
 }
