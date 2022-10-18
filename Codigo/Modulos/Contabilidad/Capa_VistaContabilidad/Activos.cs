ab1ac67ad667f8fce170d340f16dafef252e4fd9
Amaya94 <jamayao@miumg.edu.gt>
2022-10-17 22:41:42 -0600

    Actualizando MDI- Subiendo Ejecutador

---
+++ Codigo/Modulos/Contabilidad/Capa_VistaContabilidad/Activos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Capa_VistaContabilidad
+{
+    public partial class Activos : Form
+    {
+        public Activos()
+        {
+            InitializeComponent();
+        }
+
+        private void Activos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
