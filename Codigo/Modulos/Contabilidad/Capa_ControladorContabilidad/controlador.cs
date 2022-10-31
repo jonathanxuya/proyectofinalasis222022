545aa462933880f7ac4b860ea62bcbfb9aa1a325
Jonathan Xuyá <jxuyax@miumg.edu.gt>
2022-10-31 13:15:53 -0600

    Subiendo proceso de estado financiero y activos fijos

--- Codigo/Modulos/Contabilidad/Capa_ControladorContabilidad/controlador.cs
+++ Codigo/Modulos/Contabilidad/Capa_ControladorContabilidad/controlador.cs
@@ -427,6 +427,325 @@ namespace Capa_ControladorContabilidad
             }
             return dt;
         }
+        public bool InsertProducto(string _Pk, string _Edi, string _Mue, string _Equi, string _Maqui, string _Herra, string _Eqco)
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection("FIL=MS Access;DSN=colchoneria"))
+                {
+                    OdbcCommand cmd = new OdbcCommand();
+                    con.Open();
+                    cmd.Connection = con;
+
+                    #region Query
+                    string query = @"INSERT INTO tbl_activosfijos (pk_ActivosFijos,Edificaiones,Muebles,Equipos,Maquinaria,Herramientas,EquipoComputo)VALUE(?,?,?,?,?,?,?);";
+                    #endregion
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+                    cmd.Parameters.Add("@pk_ActivosFijos", OdbcType.Int).Value = _Pk;
+                    cmd.Parameters.Add("@Edificaiones", OdbcType.VarChar).Value = _Edi;
+                    cmd.Parameters.Add("@Muebles", OdbcType.VarChar).Value = _Mue;
+                    cmd.Parameters.Add("@Equipos", OdbcType.VarChar).Value = _Equi;
+                    cmd.Parameters.Add("@Maquinaria", OdbcType.VarChar).Value = _Maqui;
+                    cmd.Parameters.Add("@Herramientas", OdbcType.VarChar).Value = _Herra;
+                    cmd.Parameters.Add("@EquipoComputo", OdbcType.VarChar).Value = _Eqco;
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                    con.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+
+        public bool UpdateProducto(string _Pk, string _Edi, string _Mue, string _Equi, string _Maqui, string _Herra, string _Eqco)
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection("FIL=MS Access;DSN=colchoneria"))
+                {
+                    OdbcCommand cmd = new OdbcCommand();
+                    con.Open();
+                    cmd.Connection = con;
+
+                    #region Query
+                    string query = @"UPDATE tbl_activosfijos SET tbl_activosfijos.Edificaiones = ?, tbl_activosfijos.Muebles = ?, tbl_activosfijos.Equipos = ?, tbl_activosfijos.Maquinaria = ? , tbl_activosfijos.Herramientas = ? , tbl_activosfijos.EquipoComputo = ? WHERE tbl_activosfijos.pk_ActivosFijos = ?;";
+                    #endregion
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+
+                    cmd.Parameters.Add("@Edificaiones", OdbcType.VarChar).Value = _Edi;
+                    cmd.Parameters.Add("@Muebles", OdbcType.VarChar).Value = _Mue;
+                    cmd.Parameters.Add("@Equipos", OdbcType.VarChar).Value = _Equi;
+                    cmd.Parameters.Add("@Maquinaria", OdbcType.VarChar).Value = _Maqui;
+                    cmd.Parameters.Add("@Herramientas", OdbcType.VarChar).Value = _Herra;
+                    cmd.Parameters.Add("@EquipoComputo", OdbcType.VarChar).Value = _Eqco;
+                    cmd.Parameters.Add("@pk_ActivosFijos", OdbcType.Int).Value = _Pk;
+
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                    con.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteProducto(string _Pk)
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection("FIL=MS Access;DSN=colchoneria"))
+                {
+                    OdbcCommand cmd = new OdbcCommand();
+                    con.Open();
+                    cmd.Connection = con;
+                    #region Query
+                    string query = @"DELETE FROM tbl_activosfijos WHERE tbl_activosfijos.pk_ActivosFijos = ?;";
+                    #endregion
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+                    cmd.Parameters.Add("@pk_ActivosFijos", OdbcType.VarChar).Value = _Pk;
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.Clear();
+                    con.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public DataTable SelectProducto()
+        {
+            DataTable dt = new DataTable();
+            using (OdbcDataAdapter adapter = new OdbcDataAdapter("SELECT * FROM tbl_activosfijos;", "FIL=MS Access;DSN=colchoneria"))
+            {
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        public DataTable BuscarDato(string data, string col, DataTable dt)
+        {
+            OdbcConnection con = new OdbcConnection("Dsn=Colchoneria");
+            try
+            {
+                //DataTable dt = new DataTable();
+                String cadenaB = "";
+                con.Open();
+                cadenaB = " SELECT * FROM tbl_activosfijos WHERE " + col + " LIKE ('%" + data.Trim() + "%')";
+                OdbcDataAdapter datos = new OdbcDataAdapter(cadenaB, con);
+                datos.Fill(dt);
+                OdbcCommand comando = new OdbcCommand(cadenaB, con);
+                OdbcDataReader leer;
+                leer = comando.ExecuteReader();
+            }
+            catch
+            {
+                String textalert = " El dato : " + data + " No se encontro ";
+                MessageBox.Show(textalert);
+            }
+            con.Close();
+            return dt;
+        }
+
+        public DataTable ActualizarT(string table, DataTable dt)
+        {
+            try
+            {
+                OdbcConnection con = new OdbcConnection("Dsn=Colchoneria");
+                String cadena = "";
+                con.Open();
+                cadena = "SELECT * FROM " + table;
+                OdbcDataAdapter datos = new OdbcDataAdapter(cadena, con);
+                datos.Fill(dt);
+                OdbcCommand comando = new OdbcCommand(cadena, con);
+                OdbcDataReader leer;
+                leer = comando.ExecuteReader();
+                con.Close();
+            }
+            catch
+            {
+                String textalert = " Error al actualizar datos, puede que no haya datos que mostrar ";
+                MessageBox.Show(textalert);
+            }
+            return dt;
+        }
+        public bool InsertEstados(string _Pk, string _Edi, string _Mue, string _Equi, string _Maqui, string _Herra, string _Eqco)
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection("FIL=MS Access;DSN=colchoneria"))
+                {
+                    OdbcCommand cmd = new OdbcCommand();
+                    con.Open();
+                    cmd.Connection = con;
+
+                    #region Query
+                    string query = @"INSERT INTO tbl_EstadosFinancieros (Pk_EstadosFinancieros,ActivoCirculante,ActivoNoCirculante,PasivoACortoPlazo,PasivoALargoPlazo,CapitalContable,ResultadoAPeriodo)VALUE(?,?,?,?,?,?,?);";
+                    #endregion
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+                    cmd.Parameters.Add("@Pk_EstadosFinancieros", OdbcType.Int).Value = _Pk;
+                    cmd.Parameters.Add("@ActivoCirculante", OdbcType.VarChar).Value = _Edi;
+                    cmd.Parameters.Add("@ActivoNoCirculante", OdbcType.VarChar).Value = _Mue;
+                    cmd.Parameters.Add("@PasivoACortoPlazo", OdbcType.VarChar).Value = _Equi;
+                    cmd.Parameters.Add("@PasivoALargoPlazo", OdbcType.VarChar).Value = _Maqui;
+                    cmd.Parameters.Add("@CapitalContable", OdbcType.VarChar).Value = _Herra;
+                    cmd.Parameters.Add("@ResultadoAPeriodo", OdbcType.VarChar).Value = _Eqco;
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                    con.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+
+        public bool UpdateEstados(string _Pk, string _Edi, string _Mue, string _Equi, string _Maqui, string _Herra, string _Eqco)
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection("FIL=MS Access;DSN=colchoneria"))
+                {
+                    OdbcCommand cmd = new OdbcCommand();
+                    con.Open();
+                    cmd.Connection = con;
+
+                    #region Query
+                    string query = @"UPDATE tbl_EstadosFinancieros SET tbl_EstadosFinancieros.ActivoCirculante = ?, tbl_EstadosFinancieros.ActivoNoCirculante = ?, tbl_EstadosFinancieros.PasivoACortoPlazo = ?, tbl_EstadosFinancieros.PasivoALargoPlazo = ? , tbl_EstadosFinancieros.CapitalContable = ? , tbl_EstadosFinancieros.ResultadoAPeriodo = ? WHERE tbl_EstadosFinancieros.Pk_EstadosFinancieros = ?;";
+                    #endregion
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+
+
+                    cmd.Parameters.Add("@ActivoCirculante", OdbcType.VarChar).Value = _Edi;
+                    cmd.Parameters.Add("@ActivoNoCirculante", OdbcType.VarChar).Value = _Mue;
+                    cmd.Parameters.Add("@PasivoACortoPlazo", OdbcType.VarChar).Value = _Equi;
+                    cmd.Parameters.Add("@PasivoALargoPlazo", OdbcType.VarChar).Value = _Maqui;
+                    cmd.Parameters.Add("@CapitalContable", OdbcType.VarChar).Value = _Herra;
+                    cmd.Parameters.Add("@ResultadoAPeriodo", OdbcType.VarChar).Value = _Eqco;
+                    cmd.Parameters.Add("@Pk_EstadosFinancieros", OdbcType.Int).Value = _Pk;
+
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                    con.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteEstados(string _Pk)
+        {
+            try
+            {
+                using (OdbcConnection con = new OdbcConnection("FIL=MS Access;DSN=colchoneria"))
+                {
+                    OdbcCommand cmd = new OdbcCommand();
+                    con.Open();
+                    cmd.Connection = con;
+                    #region Query
+                    string query = @"DELETE FROM tbl_activosfijos WHERE tbl_activosfijos.pk_ActivosFijos = ?;";
+                    #endregion
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+                    cmd.Parameters.Add("@tbl_EstadosFinancieros", OdbcType.VarChar).Value = _Pk;
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.Clear();
+                    con.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public DataTable SelectEstados()
+        {
+            DataTable dt = new DataTable();
+            using (OdbcDataAdapter adapter = new OdbcDataAdapter("SELECT * FROM tbl_EstadosFinancieros;", "FIL=MS Access;DSN=colchoneria"))
+            {
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        public DataTable BuscarDato(string data, string col, DataTable dt)
+        {
+            OdbcConnection con = new OdbcConnection("Dsn=colchoneria");
+            try
+            {
+                //DataTable dt = new DataTable();
+                String cadenaB = "";
+                con.Open();
+                cadenaB = " SELECT * FROM tbl_EstadosFinancieros WHERE " + col + " LIKE ('%" + data.Trim() + "%')";
+                OdbcDataAdapter datos = new OdbcDataAdapter(cadenaB, con);
+                datos.Fill(dt);
+                OdbcCommand comando = new OdbcCommand(cadenaB, con);
+                OdbcDataReader leer;
+                leer = comando.ExecuteReader();
+            }
+            catch
+            {
+                String textalert = " El dato : " + data + " No se encontro ";
+                MessageBox.Show(textalert);
+            }
+            con.Close();
+            return dt;
+        }
+
+        public DataTable ActualizarE(string table, DataTable dt)
+        {
+            try
+            {
+                OdbcConnection con = new OdbcConnection("Dsn=colchoneria");
+                String cadena = "";
+                con.Open();
+                cadena = "SELECT * FROM " + table;
+                OdbcDataAdapter datos = new OdbcDataAdapter(cadena, con);
+                datos.Fill(dt);
+                OdbcCommand comando = new OdbcCommand(cadena, con);
+                OdbcDataReader leer;
+                leer = comando.ExecuteReader();
+                con.Close();
+            }
+            catch
+            {
+                String textalert = " Error al actualizar datos, puede que no haya datos que mostrar ";
+                MessageBox.Show(textalert);
+            }
+            return dt;
+        }
 
     }
 }
